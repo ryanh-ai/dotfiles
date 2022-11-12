19cda9880b3126b3be407c60f3507127c619e2ed
canada4663 <3118399+canada4663@users.noreply.github.com>
2022-11-12 10:09:43 -0800

    true up before laptop migration

--- home/backup.sh
+++ home/backup.sh
@@ -1,5 +1,5 @@
 duplicity inc --no-encryption --file-prefix outlook- /Users/$(whoami)/Library/Group\ Containers/UBF8T346G9.Office/Outlook/ file:///Users/rhoium/Amazon\ WorkDocs\ Drive/My\ Documents/TarBack/outlook/
 duplicity inc --no-encryption --file-prefix desktop- /Users/$(whoami)/Desktop file:///Users/rhoium/Amazon\ WorkDocs\ Drive/My\ Documents/TarBack/desktop/
-duplicity inc --no-encryption --file-prefix desktop- /Users/$(whoami)/Downloads file:///Users/rhoium/Amazon\ WorkDocs\ Drive/My\ Documents/TarBack/downloads/
+duplicity inc --no-encryption --file-prefix downloads- /Users/$(whoami)/Downloads file:///Users/rhoium/Amazon\ WorkDocs\ Drive/My\ Documents/TarBack/downloads/
 duplicity inc --no-encryption --file-prefix workplace- /Volumes/workplace file:///Users/rhoium/Amazon\ WorkDocs\ Drive/My\ Documents/TarBack/workplace/
 
